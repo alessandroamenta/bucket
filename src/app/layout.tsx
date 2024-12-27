c10df297d05f3334939fe00403b17f41bc3157a0
alessandroamenta <alessandroamenta1@gmail.com>
2024-12-27 19:54:47 +0100

    img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,46 @@
-import './globals.css'
-import { Reenie_Beanie } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react'
+import "./globals.css";
+import { Reenie_Beanie } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const reenieBeanie = Reenie_Beanie({
-  weight: '400',
-  subsets: ['latin']
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "ale's bucket list",
-  description: 'things i wanna do before i die',
-}
+  description: "things i wanna do before i die",
+  openGraph: {
+    title: "ale's bucket list",
+    description: "things i wanna do before i die",
+    images: [
+      {
+        url: "/sampleimg.png",
+        width: 1200,
+        height: 630,
+        alt: "ale's bucket list",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ale's bucket list",
+    description: "things i wanna do before i die",
+    images: ["/sampleimg.png"],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={reenieBeanie.className}>
         {children}
         <Analytics />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
