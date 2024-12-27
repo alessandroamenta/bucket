746b2205cbb1d0bcb1b1745b30f1c7160f6a867b
alessandroamenta <alessandroamenta1@gmail.com>
2024-12-27 20:10:26 +0100

    lets try again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,33 +7,34 @@ const reenieBeanie = Reenie_Beanie({
   subsets: ["latin"],
 });
 
-const defaultUrl = "https://www.alesbucket.com";
-
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
+  metadataBase: new URL("https://alesbucket.com"),
   title: "ale's bucket list",
   description: "things i wanna do before i die",
   openGraph: {
     title: "ale's bucket list",
     description: "things i wanna do before i die",
-    url: defaultUrl,
     siteName: "ale's bucket list",
+    type: "website",
     images: [
       {
-        url: `${defaultUrl}/sampleimg.png`,
+        url: "https://alesbucket.com/sampleimg.png",
         width: 1200,
         height: 630,
         alt: "ale's bucket list",
       },
     ],
-    locale: "en_US",
-    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "ale's bucket list",
     description: "things i wanna do before i die",
-    images: [`${defaultUrl}/sampleimg.png`],
+    images: ["https://alesbucket.com/sampleimg.png"],
+    creator: "@ale",
+  },
+  robots: {
+    index: true,
+    follow: true,
   },
 };
 
@@ -44,25 +45,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta property="og:title" content="ale's bucket list" />
-        <meta
-          property="og:description"
-          content="things i wanna do before i die"
-        />
-        <meta property="og:image" content={`${defaultUrl}/sampleimg.png`} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:url" content={defaultUrl} />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="ale's bucket list" />
-        <meta
-          name="twitter:description"
-          content="things i wanna do before i die"
-        />
-        <meta name="twitter:image" content={`${defaultUrl}/sampleimg.png`} />
-      </head>
       <body className={reenieBeanie.className}>
         {children}
         <Analytics />
