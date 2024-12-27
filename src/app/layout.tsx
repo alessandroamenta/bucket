f932c72c14a2ef3832190c16f4c8e5d2a1f47996
alessandroamenta <alessandroamenta1@gmail.com>
2024-12-27 21:37:13 +0100

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,24 +16,12 @@ export const metadata = {
     description: "things i wanna do before i die",
     siteName: "ale's bucket list",
     type: "website",
-    images: [
-      {
-        url: "/api/og",
-        width: 1200,
-        height: 630,
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "ale's bucket list",
     description: "things i wanna do before i die",
-    images: ["/api/og"],
     creator: "@ale",
-  },
-  robots: {
-    index: true,
-    follow: true,
   },
 };
 
